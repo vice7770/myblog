37e05680e72c31b569bf7b939c363eee9b71428b
vicente almeida <vicente.cra45@gmail.com>
2024-07-29 15:03:15 +0400

    Second changes for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
@@ -30,6 +36,17 @@ export default function HomePage() {
               deploy it.
             </div>
           </Link>
+          <div>
+          {
+            posts.map((post) => {
+              return (
+                <div key={post.id} className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
+                  <h3 className="text-2xl font-bold">{post.name}</h3>
+                </div>
+              );
+            })          
+          }
+          </div>
         </div>
       </div>
     </main>
