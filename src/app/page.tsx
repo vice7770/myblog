7c588e964bfbbdf54b8120322fd1010778389d72
vicente almeida <vicente.cra45@gmail.com>
2024-08-05 00:31:18 +0400

    Simple changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,16 +15,16 @@ export default async function HomePage() {
   const averageTemp = getWeatherAverageTemp(weatherMetaData as WeatherMetaData[]);
   const averageTempYesterday = getWeatherAverageTemp(weatherYesterdayDataData as WeatherMetaData[])
   return (
-    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#add8e6] to-[#00008b] text-white">
+    <main className="flex min-h-screen flex-col items-center bg-white text-black">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <Hero />
         <h3 className="text-2xl font-bold">Lets play a little game</h3>
         <div className="grid grid-cols-1 gap-4 md:gap-8 w-full">
           <div className="flex justify-center items-center w-full space-x-4">
-            <Button className="mb-2 h-[50px] w-[250px] rounded-3xl bg-blue-500 px-4 py-2 text-lg text-white">
+            <Button className="mb-2 h-[50px] w-[250px] rounded-3xl bg-blue-500 px-4 py-2 text-lg text-black">
               Overview
             </Button>        
-            <Button className="mb-2 h-[50px] w-[250px] rounded-3xl bg-blue-500 px-4 py-2 text-lg text-white">
+            <Button className="mb-2 h-[50px] w-[250px] rounded-3xl bg-blue-500 px-4 py-2 text-lg text-black">
               Show me the weather
             </Button>
           </div>
